fbd576960cd78a6c8f0145deff5afdefd687a57b
muhamad akbar <77180979+afmtsans@users.noreply.github.com>
2024-07-10 11:37:50 +0700

    Add files via upload

---
+++ tampil_users.php
@@ -0,0 +1,42 @@
+<div class="card">
+  <div class="card-header bg-primary text-white border-dark"><strong>Data Users</strong></div>
+  <div class="card-body">
+<a class="btn btn-primary mb-2" href="?page=users&action=tambah">Tambah</a>
+<table class="table table-bordered" id="myTable">
+    <thead>
+      <tr align="center">
+        <th width="30px">No.</th>
+        <th>Username</th>
+        <th>Role</th>
+        <th width="90px">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+		<!-- letakkan proses menampilkan disini -->
+        <?php
+            $no=1;
+            $sql = "SELECT*FROM users ORDER BY username ASC";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_assoc()) {
+        ?>
+            <tr>
+                <td align="center"><?php echo $no++; ?></td>
+                <td><?php echo $row['username']; ?></td>
+                <td><?php echo $row['role']; ?></td>
+                <td align="center">
+                    <a class="btn btn-warning" href="?page=users&action=update&id=<?php echo $row['idusers']; ?>">
+                        <i class="fas fa-edit"></i>
+                    </a>
+                    <a onclick="return confirm('Yakin menghapus data ini ?')" class="btn btn-danger" href="?page=users&action=hapus&id=<?php echo $row['idusers']; ?>">
+                        <i class="fas fa-trash-alt"></i>
+                    </a>
+                </td>
+            </tr>
+        <?php
+            }
+            $conn->close();
+        ?>
+   </tbody>
+</table>
+</div>
+</div>
